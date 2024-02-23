2a1b3b3c69c56248a584496f9822d9591cd16036
FranJos <fjchanataxi@uce.edu.ec>
2024-02-22 22:22:35 -0500

    API de autorización

---
+++ src/main/java/com/example/demo/service/to/UsuarioTo.java
@@ -0,0 +1,33 @@
+package com.example.demo.service.to;
+
+import java.io.Serializable;
+
+public class UsuarioTo implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private String nombre;
+	private String password;
+
+	@Override
+	public String toString() {
+		return "UsuarioTo [nombre=" + nombre + ", password=" + password + "]";
+	}
+
+	// SET y GET
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
